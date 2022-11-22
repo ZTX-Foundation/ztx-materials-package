8de4851552afba6f1cd95755107bc53b973e2491
Aaron van der Brugge <45329627+aaronvdbrugge@users.noreply.github.com>
2022-11-22 15:02:09 +0900

    [Style Set] Add Style A and basics

---
+++ Scripts/MaterialStyleSetSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZTX.Materials
+{
+    [CreateAssetMenu(fileName = "Material Style Set", menuName = "ZTX/Material Style Set", order = 2)]
+    public class MaterialStyleSetSO : ScriptableObject
+    {
+        [Header("Body")]
+        public Material body;
+        public Material eye;
+        public Material eyelash;
+        public Material hair;
+        
+        [Header("Clothing")]
+        public Material standard;
+        public Material standardTransparent;
+        public Material standardTwoSide;
+        public Material fur;
+    }
+}
---
+++ Scripts/StyleSetsSO.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZTX.Materials
+{
+    [CreateAssetMenu(fileName = "Style Sets", menuName = "ZTX/Style Sets", order = 1)]
+    public class StyleSetsSO : ScriptableObject
+    {
+        public MaterialStyleSetSO[] styleSets;
+
+        // Swap all the relevant materials of target gameObject to the new material styleset
+        public void SetMaterials(GameObject target, int styleSetIndex)
+        {
+            if (styleSetIndex > styleSets.Length)
+            {
+                Debug.LogError("styleSetIndex out of bounds");
+            }
+
+            MaterialStyleSetSO styleSet = styleSets[styleSetIndex];
+
+            SkinnedMeshRenderer[] skinnedMeshRenderers = target.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+            for (int i = 0; i < skinnedMeshRenderers.Length; i++)
+            {
+                SkinnedMeshRenderer s = skinnedMeshRenderers[i];
+                Material[] materials = s.materials;
+
+                for (int j = 0; j < materials.Length; j++)
+                {
+                    Material m = materials[j];
+                        
+                    // Replace the material
+                    m = SwapMaterial(m, styleSet);
+
+                    materials[j] = m;
+                }
+
+                s.materials = materials;
+            }
+        }
+
+        private Material SwapMaterial(Material m, MaterialStyleSetSO styleSet)
+        {
+            if (m.name.Contains("body"))
+                return styleSet.body;
+            if (m.name.Contains("eyelash"))
+                return styleSet.eyelash;
+            if (m.name.Contains("eye"))
+                return styleSet.eye;
+            if (m.name.Contains("standardTransparent"))
+                return styleSet.standardTransparent;
+            if (m.name.Contains("standardTwoSide"))
+                return styleSet.standardTwoSide;
+            if (m.name.Contains("standard"))
+                return styleSet.standard;
+            if (m.name.Contains("fur"))
+                return styleSet.fur;
+            Debug.LogError("No replacing material found");
+            return null;
+        }
+    }
+}

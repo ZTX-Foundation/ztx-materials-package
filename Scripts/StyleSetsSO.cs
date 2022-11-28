f1304866c6473f4f55fc54217f6dbe6fb3e38c16
Aaron van der Brugge <45329627+aaronvdbrugge@users.noreply.github.com>
2022-11-28 18:53:01 +0900

    Update StyleSetsSO.cs

--- Scripts/StyleSetsSO.cs
+++ Scripts/StyleSetsSO.cs
@@ -52,6 +52,11 @@ namespace ZTX.Materials
                             Color color = m.GetColor("_BaseColor");
                             mNew.SetColor(styleset.mainColorName, color);
                             Debug.Log($"_BaseColor - {color} - {styleset.mainColorName} - {m.name} - {mNew.name}");
+                        } else if (m.HasProperty("_Color"))
+                        {
+                            Color color = m.GetColor("_Color");
+                            mNew.SetColor(styleset.mainColorName, color);
+                            Debug.Log($"_Color - {color} - {styleset.mainColorName} - {m.name} - {mNew.name}");
                         }
 
                         m = mNew;
